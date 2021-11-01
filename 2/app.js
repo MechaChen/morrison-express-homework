c25a0bcda516db74b1deb8622ed175034a9efeec
Benson <tumlivein@gmail.com>
2021-11-01 17:30:01 +0800

    2 : 跳過 disabled 的 checkbox

--- 2/app.js
+++ 2/app.js
@@ -1,67 +1,54 @@
 const list = [
     {
         isCheckable: true,
-        checked: false,
         name: '00-未派車',
     },
     {
-        isCheckable: true,
-        checked: false,
+        isCheckable: false,
         name: '01-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '02-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '03-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '04-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '05-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '06-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '07-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '08-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '09-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '10-未派車',
     },
     {
-        isCheckable: true,
-        checked: false,
+        isCheckable: false,
         name: '11-未派車',
     },
     {
         isCheckable: true,
-        checked: false,
         name: '12-未派車',
     },
 ]
@@ -79,8 +66,7 @@ function App() {
                     isInBetween = !isInBetween
                 }
 
-                if (isInBetween) {
-                    console.log('e.target is in between=>', e.target)
+                if (isInBetween && !checkbox.disabled) {
                     checkbox.checked = true
                 }
             })
@@ -92,10 +78,14 @@ function App() {
     return (
         <div className="inbox">
             {list.map((item, i) => (
-                <label key={i} className="item">
-                    <input type="checkbox" onClick={handleClick} />
+                <div key={i} className="item">
+                    <input
+                        type="checkbox"
+                        onClick={handleClick}
+                        disabled={!item.isCheckable}
+                    />
                     <p>{item.name}</p>
-                </label>
+                </div>
             ))}
         </div>
     )
